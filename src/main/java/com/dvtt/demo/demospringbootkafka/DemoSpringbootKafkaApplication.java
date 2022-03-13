66683a4649703a7a32c1eb3c441cae257f4d2550
link2mountain <linhtn@vnpay.vn>
2022-03-13 22:11:49 +0700

    first commit

---
+++ src/main/java/com/dvtt/demo/demospringbootkafka/DemoSpringbootKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.dvtt.demo.demospringbootkafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringbootKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringbootKafkaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dvtt/demo/demospringbootkafka/DemoSpringbootKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dvtt.demo.demospringbootkafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringbootKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
